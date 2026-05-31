98bb09066ffe5c63292adb2fe1eec28f25ee7830
Santanu Banerjee <santanubanerjee490@gmail.com>
2026-05-31 09:18:17 +0530

    Add files via upload

---
+++ N_0/Intermediate_Solutions/_0.sol
@@ -0,0 +1,3 @@
+# Solution for model Super-Permutation_0
+# Objective value = 0
+z[(),1] 1
---
+++ N_1/Intermediate_Solutions/_0.sol
@@ -0,0 +1,8 @@
+# Solution for model Super-Permutation_1
+# Objective value = 1
+x[1] 1
+f[1] 1
+y[(1,),1,1] 0
+g[(1,),1,1] 0
+h[(1,),1,1] 1
+z[(1,),1] 1
